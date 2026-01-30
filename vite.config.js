0b79c44a5fbf59563574f51c642937bb9155b836
Anomi-LCB <dlckdqja710@gmail.com>
2026-01-30 22:10:48 +0900

    Ensure all UI and Meta changes are synced to SUNLIST-1

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/SUNLIST/', // GitHub Pages base path (must match repo name)
+    base: '/SUNLIST-1/', // GitHub Pages base path (must match repo name)
     build: {
         outDir: 'dist',
         emptyOutDir: true,
